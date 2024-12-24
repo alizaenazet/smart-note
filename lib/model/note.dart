f8b1a4d3bdae1f04d100c690065c7d3e6f340fd4
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-12-24 11:08:34 +0700

    feat: enhance Note and Task models with JSON serialization and optional fields

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,29 +1,95 @@
-//class sementara karna backend blm selesai spy ga error viewnya
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_note/model/task.dart';
 
-class Note {
-  final String id;
-  final String title;
-  final String description;
-  final String date;
-  final List<Task> tasks;
-  final IconData icon;
+class Note extends Equatable {
+  final String? id;
+  final String? title;
+  final String? content;
+  final String? updatedAt;
+  final String? icon;
+  final bool? isComplete;
+  final List<Task>? todoList;
 
-  Note({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.date,
-    required this.tasks,
-    required this.icon,
+  const Note({
+    this.id,
+    this.title,
+    this.content,
+    this.updatedAt,
+    this.icon,
+    this.isComplete,
+    this.todoList,
   });
 
+  factory Note.fromMap(Map<String, dynamic> data) => Note(
+        id: data['id'] as String?,
+        title: data['title'] as String?,
+        content: data['content'] as String?,
+        updatedAt: data['updatedAt'] as String?,
+        icon: data['icon'] as String?,
+        isComplete: data['isComplete'] as bool?,
+        todoList: (data['todoList'] as List<dynamic>?)
+            ?.map((e) => Task.fromMap(e as Map<String, dynamic>))
+            .toList(),
+      );
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'title': title,
+        'content': content,
+        'updatedAt': updatedAt,
+        'icon': icon,
+        'isComplete': isComplete,
+        'todoList': todoList?.map((e) => e.toMap()).toList(),
+      };
+
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [Note].
+  factory Note.fromJson(String data) {
+    return Note.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
+
+  /// `dart:convert`
+  ///
+  /// Converts [Note] to a JSON string.
+  String toJson() => json.encode(toMap());
+
+  @override
+  List<Object?> get props {
+    return [
+      id,
+      title,
+      content,
+      updatedAt,
+      icon,
+      isComplete,
+      todoList,
+    ];
+  }
+
+  IconData get getIcon {
+    switch (icon) {
+      case 'work':
+        return Icons.work;
+      case 'study':
+        return Icons.menu_book;
+      case 'home':
+        return Icons.home;
+      case 'others':
+        return Icons.category;
+      default:
+        return Icons.note_alt_outlined;
+    }
+  }
+
   bool get isCompleted {
-    return tasks.every((task) => task.isCompleted);
+    return todoList!.every((task) => task.isCompleted!);
   }
 
   List<Task> getCompletedTasks() {
-    return tasks.where((task) => task.isCompleted).toList();
+    return todoList!.where((task) => task.isCompleted!).toList();
   }
 }
--- lib/model/task.dart
+++ lib/model/task.dart
@@ -1,11 +1,38 @@
-//class sementara karna backend blm selesai spy ga error viewnya
+import 'dart:convert';
 
-class Task {
-  String description;
-  bool isCompleted;
+import 'package:equatable/equatable.dart';
 
-  Task({
-    required this.description,
-    this.isCompleted = false,
-  });
+class Task extends Equatable {
+  final int? id;
+  final String? todo;
+  bool? isCompleted;
+
+  Task({this.id, this.todo, this.isCompleted});
+
+  factory Task.fromMap(Map<String, dynamic> data) => Task(
+        id: data['id'] as int?,
+        todo: data['todo'] as String?,
+        isCompleted: data['isCompleted'] as bool?,
+      );
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'todo': todo,
+        'isCompleted': isCompleted,
+      };
+
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [Task].
+  factory Task.fromJson(String data) {
+    return Task.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
+
+  /// `dart:convert`
+  ///
+  /// Converts [Task] to a JSON string.
+  String toJson() => json.encode(toMap());
+
+  @override
+  List<Object?> get props => [id, todo, isCompleted];
 }
