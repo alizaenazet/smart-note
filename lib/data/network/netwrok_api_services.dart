d865273662e86ba0a683cd4eaad438a9fb8f90a8
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2024-12-24 11:07:27 +0700

    feat: implement API service layer with exception handling and response management

---
+++ lib/data/network/netwrok_api_services.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:smart_note/data/network/base_api_services.dart';
+import 'package:smart_note/shared/shared.dart';
+
+import '../app_exception.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkApiServices implements BaseApiServices {
+  @override
+  Future deleteApiResponse(String url) {
+    // TODO: implement deleteApiResponse
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<http.Response> getApiResponse(String endpoint,
+      {Map<String, dynamic>? queryParams}) async {
+    try {
+      final queryParamsWithKey = {
+        ...?queryParams,
+      };
+      final uri =
+          Uri.https(Const.smartNoteBaseUrl, endpoint, queryParamsWithKey);
+      final response = await http.get(
+        uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+      );
+      debugPrint('Response [$endpoint]: $response');
+      return returnResponse(response);
+    } on SocketException {
+      throw NoInternetException('');
+    } on TimeoutException {
+      throw FetchDataException('Api not responding');
+    }
+  }
+
+  @override
+  Future postApiResponse(String endpoint, data,
+      {Map<String, dynamic>? queryParams}) async {
+    try {
+      final queryParamsWithKey = {
+        ...?queryParams,
+      };
+      final uri =
+          Uri.https(Const.smartNoteBaseUrl, endpoint, queryParamsWithKey);
+      final response = await http.post(
+        uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: data != null ? jsonEncode(data) : null,
+      );
+      debugPrint('Response [$endpoint]: $response');
+      return returnResponse(response);
+    } on SocketException {
+      throw NoInternetException('');
+    } on TimeoutException {
+      throw FetchDataException('Api not responding');
+    }
+  }
+
+  @override
+  Future putApiResponse(String url, data) {
+    // TODO: implement putApiResponse
+    throw UnimplementedError();
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 201:
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 500:
+      case 404:
+        throw UnauthorisedException(response.body.toString());
+      default:
+        throw FetchDataException(
+            'Error occured while communicating with server');
+    }
+  }
+}
